1a2ec3245622edb1efe3cb0b9c9accc04e61c266
Mauro-Pereira <mauropereira1096@gmail.com>
2021-04-08 21:14:40 -0300

    import routes and bodyParser in this file

--- CRUD/src/index.ts
+++ CRUD/src/index.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata";
 import * as express from 'express';
-
+import * as bodyParser from 'body-parser';
+import routes from './route';
 const app = express();
 
-
+app.use(bodyParser.json());
+app.use(routes);
 
 app.listen(3333,()=>{
     console.log("running in port 3333");
